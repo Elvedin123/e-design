e57a95a82b1bc4d6e7aee2bd8112ed56c9e37968
elvedin123 <elvedinc97@gmail.com>
2023-02-07 17:37:13 -0500

    adding firebase utilities

--- src/routes/sign-in/signin.component.jsx
+++ src/routes/sign-in/signin.component.jsx
@@ -1,3 +1,4 @@
+
 const Signin = () => {
   return (
     <div>
---
+++ src/utilities/firebase/firebase.utils.jsx
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHVp3bap1pFROgzdcpk-GAIebdcRV2AzQ",
+  authDomain: "e-design-db.firebaseapp.com",
+  projectId: "e-design-db",
+  storageBucket: "e-design-db.appspot.com",
+  messagingSenderId: "702213551973",
+  appId: "1:702213551973:web:a8a25da923e16c76eed38c"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
