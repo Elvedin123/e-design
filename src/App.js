22ba2c12fbd5bda3a1cc93f93ff7bfd5e1c5c1cd
elvedin123 <elvedinc97@gmail.com>
2023-02-27 21:01:40 -0500

    working on checkout page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./routes/home/home.component";
 import Nav from "./routes/navbar/nav.component";
 import Shop from "./routes/shop/shop.component";
 import Authentication from "./routes/authentication/authentication.component";
+import Checkout from "./routes/checkout/checkout.component";
 import { Routes, Route } from "react-router-dom";
 
 
@@ -15,6 +16,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<Checkout />} />
       </Route>
 
     </Routes>
---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,32 @@
+import './checkout.styles.scss'
+import { useContext } from 'react'
+import { CartContext } from '../../contexts/cart.context'
+
+
+const Checkout = () => {
+
+  const { cartItems, addItemToCart } = useContext(CartContext)
+
+  return (
+    <div>
+      <h1>Checkout page</h1>
+      <div>
+
+        {cartItems.map((cartItem) => {
+          const { id, name, quantity } = cartItem
+          const increment = () => addItemToCart(cartItem)
+          return <div key={id}>
+            <h2>{name}</h2>
+            <span>{quantity}</span>
+            <br />
+            <span>decrement</span>
+            <br />
+            <span onClick={increment}>increment</span>
+          </div>
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Checkout
