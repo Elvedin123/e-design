5f013bb74c8c01908a3e4049b5c80415061f8f84
elvedin123 <elvedinc97@gmail.com>
2023-02-27 20:45:44 -0500

    added cart dropdown functionality that shows new added items and allowed the cart icon to store the total quantitiy of items added

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { UserProvider } from './contexts/user.context';
 import { ProductsProvider } from './contexts/products.context';
 import reportWebVitals from './reportWebVitals';
 import { CartProvider } from './contexts/cart.context';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
